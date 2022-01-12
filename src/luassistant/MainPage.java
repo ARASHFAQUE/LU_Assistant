a94cbaab1e26c01a50bb305513a37a0b5f3a6449
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2022-01-12 09:35:54 +0600

    First Commit

--- src/luassistant/MainPage.java
+++ src/luassistant/MainPage.java
@@ -2,7 +2,10 @@ package luassistant;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MainPage extends JFrame {
@@ -26,13 +29,18 @@ public class MainPage extends JFrame {
 
         buttons = new JButton[10];
 
+        // First Section
+
         label1 = new JLabel("Student");
         label1.setFont(font1);
         label1.setBounds(120, 70, 130, 60);
 
+        // First Panel
+
         panel1 = new JPanel();
         panel1.setLayout(null);
         panel1.setBounds(50, 150, 280, 400);
+        panel1.setBackground(Color.PINK);
 
         buttons[0] = new JButton("Your Information");
         buttons[0].setFont(font2);
@@ -42,62 +50,75 @@ public class MainPage extends JFrame {
         buttons[1].setFont(font2);
         buttons[1].setBounds(15, 210, 250, 80);
 
-        panel1.setBackground(Color.PINK);
         panel1.add(buttons[0]);
         panel1.add(buttons[1]);
+
+        // Second Section
 
         label2 = new JLabel("Teacher");
         label2.setFont(font1);
         label2.setBounds(435, 70, 130, 60);
 
+        // Second Panel
+
         panel2 = new JPanel();
+        panel2.setLayout(null);
         panel2.setBounds(360, 150, 280, 400);
         panel2.setBackground(Color.cyan);
+
+        buttons[2] = new JButton("Teacher Information");
+        buttons[2].setFont(font2);
+        buttons[2].setBounds(15, 150, 250, 80);
+
+        panel2.add(buttons[2]);
+
+        // Third Section
 
         label3 = new JLabel("Others");
         label3.setFont(font1);
         label3.setBounds(745, 70, 130, 60);
 
+        // Third Panel
+
         panel3 = new JPanel();
+        panel3.setLayout(null);
         panel3.setBounds(670, 150, 280, 400);
         panel3.setBackground(Color.GREEN);
 
+        buttons[3] = new JButton("Class Routine");
+        buttons[3].setFont(font2);
+        buttons[3].setBounds(15, 100, 250, 80);
+
+        buttons[4] = new JButton("Bus Schedule");
+        buttons[4].setFont(font2);
+        buttons[4].setBounds(15, 210, 250, 80);
+
+        panel3.add(buttons[3]);
+        panel3.add(buttons[4]);
 
         // Creating a Menu Bar
         menuBar = new JMenuBar();
 
         // Placing The Menus of the Menu Bar
-        menu1 = new JMenu("Student");
-        menu2 = new JMenu("Teacher");
-        menu3 = new JMenu("Routine");
-        menu4 = new JMenu("Bus Schedule");
-        menu5 = new JMenu("Exit");
+        menu1 = new JMenu("Exit");
 
-        // Placing the Menu items into specific Menu Bar
-        menuItem1 = new JMenuItem("Your Information");
-        menuItem2 = new JMenuItem("Other Student Information");
+        menu1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                try {
+                    setVisible(false);
+                    connection.close();
+                    statement.close();
+                    System.exit(0);
+                } catch (SQLException ex) {
+                    //ex.printStackTrace();
+                }
+            }
+        });
 
-        menu1.add(menuItem1);
-        menu1.add(menuItem2);
-
-        menuItem3 = new JMenuItem("Teacher Information");
-        menu2.add(menuItem3);
-
-        menuItem4 = new JMenuItem("Download");
-        menu3.add(menuItem4);
-
-        menuItem5 = new JMenuItem("Download");
-        menu4.add(menuItem5);
-
-        menuItem6 = new JMenuItem("Exit");
-        menu5.add(menuItem6);
 
         // Adding the menus
         menuBar.add(menu1);
-        menuBar.add(menu2);
-        menuBar.add(menu3);
-        menuBar.add(menu4);
-        menuBar.add(menu5);
 
         setJMenuBar(menuBar);
 
